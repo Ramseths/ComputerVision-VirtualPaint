95afb456a6dacdfd41c6c635d7e6623056fef742
Ramseths <juan.ramseths@gmail.com>
2021-06-15 18:45:50 -0500

    Agregando lista de colores a detectar

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+
 frameWidth = 640
 frameHeight = 480
 # 1 para camaras externas, 0 para camara integrada
@@ -6,6 +8,17 @@ cap = cv2.VideoCapture(0)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10, 150)
+
+lista_colores = [[5, 107, 0, 19, 255, 255],
+                 [133, 56, 0, 159, 156, 255],
+                 [57, 76, 0, 100, 255, 255]]
+
+def encontrarColor(img):
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imgHSV, lower, upper)
+    cv2.imshow("Mask", mask)
 
 while True:
     success, img = cap.read()
