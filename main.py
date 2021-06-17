4799b9605e88ad31d458387a6af0471664db0a15
Ramseths <juan.ramseths@gmail.com>
2021-06-16 19:48:32 -0500

    Detectando contorno del objeto

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ cap = cv2.VideoCapture(0)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10, 150)
-
+# 115 36 23 135 255 200
 # Se crea la lista con los colores necesarios
-lista_colores = [[5, 107, 0, 19, 255, 255],
-                 [133, 56, 0, 159, 156, 255],
-                 [57, 76, 0, 100, 255, 255]]
+lista_colores = [[115, 36, 23, 135, 255, 200]]
 
 def encontrarColor(img, lista_colores):
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -20,11 +18,24 @@ def encontrarColor(img, lista_colores):
         lower = np.array(color[0:3])
         upper = np.array(color[3:6])
         mask = cv2.inRange(imgHSV, lower, upper)
-        cv2.imshow(str(color[0]), mask)
+        obtenerContornos(mask)
+        #cv2.imshow(str(color[0]), mask)
+
+
+def obtenerContornos(img):
+    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area>500:
+            cv2.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
+            peri = cv2.arcLength(cnt,True)
+            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
+            x, y, w, h = cv2.boundingRect(approx)
 
 while True:
     success, img = cap.read()
+    imgResult = img.copy()
     encontrarColor(img, lista_colores)
-    cv2.imshow("Salida", img)
+    cv2.imshow("Salida", imgResult)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
